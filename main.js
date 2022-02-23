73d173ff80aeb66e7f8bcdf6e181014bb877d323
hangbokancoding <79822247+hangbokancoding@users.noreply.github.com>
2022-02-23 15:58:00 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+let lineNum = 0;
+let value = "";
+let word = "";
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
+}
+word = data[getRandomInt(0, data.length)].toUpperCase();
+
+setInterval(() => {
+  if (value.length > 5) value = value.substring(0, 5);
+
+  let tds = document.querySelectorAll(
+    `#word tr:nth-child(${lineNum + 1}) > td`
+  );
+
+  for (let i = 0; i < 5; i++) {
+    tds[i].textContent = "";
+    if (value.length > i) tds[i].textContent = value[i].toUpperCase();
+  }
+});
+
+function input(a) {
+  value += a;
+}
+
+function enter() {
+  let tds = document.querySelectorAll(
+    `#word tr:nth-child(${lineNum + 1}) > td`
+  );
+
+  for (let i = 0; i < 5; i++) {
+    tds[i].style.color = "white";
+    let textContent = tds[i].textContent;
+
+    if (textContent == "") continue;
+    else if (textContent == word[i]) tds[i].style.backgroundColor = "#6aaa64";
+    // green
+    else if (word.indexOf(textContent) > -1)
+      tds[i].style.backgroundColor = "#c9b458";
+    // yellow
+    else tds[i].style.backgroundColor = "#787c7e"; // gray
+  }
+
+  lineNum++;
+  value = "";
+}
